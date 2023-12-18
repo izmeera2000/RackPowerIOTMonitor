a70f7906cee04ee77df515d48cce5d6975edcfe5
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-12-18 17:36:06 +0800

    asdasdas
    dasdasdasdasdasd

--- index.php
+++ index.php
@@ -12,8 +12,10 @@
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -->
+<?php $pagetitle = "dashboard"?>
 <!DOCTYPE html>
 <html lang="en">
+<!-- <?php echo $pagetitle; ?> -->
 
 <?php include('head.php') ?>
 
---
+++ log.php
@@ -0,0 +1,191 @@
+<!--
+=========================================================
+* Soft UI Dashboard - v1.0.7
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<?php $pagetitle = "log" ;?>
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- <?php echo $pagetitle; ?> -->
+<?php include('head.php') ?>
+
+
+<body class="g-sidenav-show  bg-gray-100">
+  <?php include('sidenav.php') ?>
+  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+    <!-- Navbar -->
+    <?php include('topnav.php') ?>
+
+    <!-- End Navbar -->
+    <div class="container-fluid py-4">
+      <div class="row">
+      <div class="col-11">
+          <div class="card ">
+          <div class="card-header ">
+              <div class="row">
+                <div class="col-lg-6 col-7">
+                  <h6>Data Incoming</h6>
+                  <p class="text-sm mb-0">
+                    <!-- <i class="fa fa-check text-info" aria-hidden="true"></i> -->
+                    <!-- <span class="font-weight-bold ms-1">30 done</span> this month -->
+                  </p>
+                </div>
+                <div class="col-lg-6 col-5 my-auto text-end">
+                  <div class="dropdown float-lg-end pe-4">
+                    <a class="cursor-pointer" id="dropdownTable" data-bs-toggle="dropdown" aria-expanded="false">
+                      <i class="fa fa-ellipsis-v text-secondary"></i>
+                    </a>
+                    <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable">
+                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Tarikh</a></li>
+                      <!-- <li><a class="dropdown-item border-radius-md" href="javascript:;">Another action</a></li> -->
+                      <!-- <li><a class="dropdown-item border-radius-md" href="javascript:;">Something else here</a></li> -->
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card-body ">
+              <div class="chart">
+                <canvas id="chart-line" class="chart-canvas" height="300"></canvas>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      
+      
+      <?php include('footer.php') ?>
+    </div>
+  </main>
+  <?php include('corejs.php') ?>
+<script>
+// var run = setInterval(replay, 1000)
+
+var ctx2 = document.getElementById("chart-line").getContext("2d");
+var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+    gradientStroke1.addColorStop(1, 'rgba(203,12,159,0.2)');
+    gradientStroke1.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+    gradientStroke1.addColorStop(0, 'rgba(203,12,159,0)'); //purple colors
+
+    var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+    gradientStroke2.addColorStop(1, 'rgba(20,23,39,0.2)');
+    gradientStroke2.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+    gradientStroke2.addColorStop(0, 'rgba(20,23,39,0)'); //purple colors
+
+    new Chart(ctx2, {
+      type: "line",
+      data: {
+        labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        datasets: [{
+            label: "Voltage In(V)",
+            tension: 0.4,
+            borderWidth: 0,
+            pointRadius: 0,
+            borderColor: "#cb0c9f",
+            borderWidth: 3,
+            backgroundColor: gradientStroke1,
+            fill: true,
+            data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+            maxBarThickness: 6
+
+          },
+          {
+            label: "Voltage Out(V)",
+            tension: 0.4,
+            borderWidth: 0,
+            pointRadius: 0,
+            borderColor: "#3A416F",
+            borderWidth: 3,
+            backgroundColor: gradientStroke2,
+            fill: true,
+            data: [30, 90, 40, 140, 290, 290, 340, 230, 400],
+            maxBarThickness: 6
+          },
+          {
+            label: "Suhu(°C)",
+            tension: 0.4,
+            borderWidth: 0,
+            pointRadius: 0,
+            borderColor: "#fbcf33",
+            borderWidth: 3,
+            backgroundColor: gradientStroke2,
+            fill: true,
+            data: [30, 25, 30, 23, 23, 25, 24, 24, 26],
+            maxBarThickness: 6
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: true,
+          }
+        },
+        interaction: {
+          intersect: false,
+          mode: 'index',
+        },
+        scales: {
+          y: {
+            grid: {
+              drawBorder: true,
+              display: true,
+              drawOnChartArea: true,
+              drawTicks: false,
+              borderDash: [5, 5]
+            },
+            ticks: {
+              display: true,
+              padding: 10,
+              color: '#b2b9bf',
+              font: {
+                size: 11,
+                family: "Open Sans",
+                style: 'normal',
+                lineHeight: 2
+              },
+            }
+          },
+          x: {
+            grid: {
+              drawBorder: false,
+              display: false,
+              drawOnChartArea: false,
+              drawTicks: false,
+              borderDash: [5, 5]
+            },
+            ticks: {
+              display: true,
+              color: '#b2b9bf',
+              padding: 20,
+              font: {
+                size: 11,
+                family: "Open Sans",
+                style: 'normal',
+                lineHeight: 2
+              },
+            }
+          },
+        },
+      },
+    });
+</script>
+
+</body>
+
+</html>
