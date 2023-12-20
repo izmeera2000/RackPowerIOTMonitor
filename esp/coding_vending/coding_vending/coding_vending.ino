7dda63196380bd1639f1e70d354d87c61b7ae434
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-12-21 01:42:20 +0800

    asd
    asdasdasdasds

--- esp/coding_vending/coding_vending/coding_vending.ino
+++ esp/coding_vending/coding_vending/coding_vending.ino
@@ -225,6 +225,34 @@ void GetSTOCK() {
         Serial.println(httpResponseCode);
         String payload = http.getString();
         Serial.println(payload);
+
+        JSONVar myObject = JSON.parse(payload);
+  
+      // JSON.typeof(jsonVar) can be used to get the type of the var
+      if (JSON.typeof(myObject) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return;
+      }
+    
+      Serial.print("JSON object = ");
+      Serial.println(myObject);
+    
+      // myObject.keys() can be used to get an array of all the keys in the object
+      JSONVar keys = myObject.keys();
+    
+      for (int i = 0; i < keys.length(); i++) {
+        JSONVar value = myObject[keys[i]];
+        Serial.print(keys[i]);
+        Serial.print(" = ");
+        Serial.println(value);
+        sensorReadingsArr[i] = double(value);
+      }
+      Serial.print("1 = ");
+      Serial.println(sensorReadingsArr[0]);
+      Serial.print("2 = ");
+      Serial.println(sensorReadingsArr[1]);
+      Serial.print("3 = ");
+      Serial.println(sensorReadingsArr[2]);
       }
       else {
         Serial.print("Error code: ");
